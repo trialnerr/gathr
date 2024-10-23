b4eecfc89f42f39672292a8f4b96f0803592762d
trialnerr <54381853+trialnerr@users.noreply.github.com>
2024-10-23 16:46:16 -0400

    setup barebones login page

---
+++ app/login.tsx
@@ -0,0 +1,11 @@
+import FormInput from "@/components/FormInput";
+import { View } from "react-native";
+
+export default function Login() {
+  <View>
+    <FormInput
+      labelValue='Enter your email'
+      placeholderText='Email'
+    ></FormInput>
+  </View>;
+}
