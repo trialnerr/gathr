b1034dc64b2d01183ef6c6b677377fdd1315c9df
trialnerr <54381853+trialnerr@users.noreply.github.com>
2024-10-23 17:38:48 -0400

    Add email, password inputs and form button

--- app/login.tsx
+++ app/login.tsx
@@ -1,11 +1,60 @@
+import FormButton from "@/components/FormButton";
 import FormInput from "@/components/FormInput";
-import { View } from "react-native";
+import { useState } from "react";
+import { StyleSheet, Text, View, TouchableOpacity} from "react-native";
 
 export default function Login() {
-  <View>
-    <FormInput
-      labelValue='Enter your email'
-      placeholderText='Email'
-    ></FormInput>
-  </View>;
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  console.log({ email, password});
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Welcome to gathr!</Text>
+      <FormInput
+        labelValue={email}
+        placeholderText='Enter your Email'
+        onChangeText={(userEmail) => setEmail(userEmail)}
+        keyboardType='email-address'
+        autoCorrect={false}
+      ></FormInput>
+      <FormInput
+        labelValue={password}
+        placeholderText='Enter your Password'
+        onChangeText={(userPassword) => setPassword(userPassword)}
+        secureTextEntry={true}
+      ></FormInput>
+      <FormButton buttonText='Login' onPress={() => alert('login button')} />
+      <TouchableOpacity
+        style={styles.navButton}
+        // onPress={() => navigation.navigate('Signup')}
+      >
+        <Text style={styles.navButtonText}>New user? Join here</Text>
+      </TouchableOpacity>
+    </View>
+  );
 }
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '',
+    alignItems: 'center',
+    padding: 20,
+  },
+  text: {
+    fontSize: 28,
+    marginBottom: 10,
+    color: '',
+  },
+  navButton: {
+    marginTop: 15,
+  },
+  navButtonText: {
+    fontSize: 18,
+    fontWeight: '500',
+    color: '',
+  },
+
+ 
+})
