b907a9c17e026d21733a570b3f985afb2320e7ae
trialnerr <54381853+trialnerr@users.noreply.github.com>
2024-10-23 16:43:14 -0400

    Include react/prop-types in react rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,5 +10,11 @@ export default [
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
-  {rules: {"react/react-in-jsx-scope": "off"}},
+  {
+    rules: {
+      "react/react-in-jsx-scope": "off",
+      "react/prop-types": "off",
+      }
+  },
+  {settings: { react: { version: "detect" } }},
 ];
