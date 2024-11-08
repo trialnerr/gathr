74f9134659eed550e08b1b5bdc11a2ae4d16c06b
trialnerr <54381853+trialnerr@users.noreply.github.com>
2024-11-08 11:41:42 -0500

    Implement Supabase authentication in login component

--- app/login.tsx
+++ app/login.tsx
@@ -1,32 +1,53 @@
 import FormButton from '@/components/FormButton';
 import FormInput from '@/components/FormInput';
+import { supabase } from '@/utils/supabase';
 import { useState } from 'react';
-import { StyleSheet, Text, View, TouchableOpacity, Alert } from 'react-native';
-// import axios from 'axios';
+import { StyleSheet, Text, View, TouchableOpacity, Alert, AppState } from 'react-native';
+
+ // Tells Supabase Auth to continuously refresh the session automatically if
+  // the app is in the foreground. When this is added, you will continue to receive
+  // `onAuthStateChange` events with the `TOKEN_REFRESHED` or `SIGNED_OUT` event
+  // if the user's session is terminated. This should only be registered once.
+  AppState.addEventListener('change', (state) => {
+    if (state === 'active') {
+      supabase.auth.startAutoRefresh();
+    } else {
+      supabase.auth.stopAutoRefresh();
+    }
+  });
 
 export default function Login() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [loading, setLoading] = useState(false);
 
-  const handleLogin = async () => {
-    try {
-      // const response = await fetch('https://localhost:5001/api/users', {
-      //   method: 'POST',
-      //   headers: {
-      //     'Content-Type': 'application/json',
-      //   },
-      //   body: JSON.stringify({ email, password }),
-      // });
-      const response = await fetch('https://swapi.dev/api/people/1');
-      if (!response.ok) {
-        Alert.alert('Error', 'err response not ok');
-      }
-      const data = await response.json();
-      Alert.alert('Success', data);
-    } catch (error) {
-      Alert.alert('Error', `Something went wrong, ${error}`);
+    async function signInWithEmail() {
+      setLoading(true);
+      const { error } = await supabase.auth.signInWithPassword({
+        email: email,
+        password: password,
+      });
+
+      if (error) Alert.alert(error.message);
+      setLoading(false);
     }
-  };
+  
+    async function signUpWithEmail() {
+      setLoading(true);
+      const {
+        data: { session },
+        error,
+      } = await supabase.auth.signUp({
+        email: email,
+        password: password,
+      });
+
+      if (error) Alert.alert(error.message);
+      if (!session)
+        Alert.alert('Please check your inbox for email verification!');
+      setLoading(false);
+    }
+
   console.log({ email, password });
   return (
     <View style={styles.container}>
@@ -44,11 +65,8 @@ export default function Login() {
         onChangeText={(userPassword) => setPassword(userPassword)}
         secureTextEntry={true}
       ></FormInput>
-      <FormButton buttonText='Login' onPress={handleLogin} />
-      <TouchableOpacity
-        style={styles.navButton}
-        // onPress={() => navigation.navigate('Signup')}
-      >
+      <FormButton buttonText='Login' onPress={signInWithEmail} />
+      <TouchableOpacity style={styles.navButton} onPress={signUpWithEmail}>
         <Text style={styles.navButtonText}>New user? Join here</Text>
       </TouchableOpacity>
     </View>
